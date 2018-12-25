773467c1def28c6ba550e1317e658fdeefa2bd66
sopan1234 <sopanrane@gmail.com>
2018-12-25 18:25:09 +0530

    xjsxkj

---
+++ src/com/StartEx.java
@@ -0,0 +1,27 @@
+package com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class StartEx {
+	public static void main(String[] args) throws Exception {
+		System.out.println(1);
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc::Mysql://localhost:3360/test","root","root");
+		Statement stmt=con.createStatement();
+		String sql="Select * From Student";
+		ResultSet rs=stmt.executeQuery(sql);
+		while(rs.next())
+		{
+			String sid=rs.getString(1);
+			System.out.println(sid);
+			String sname=rs.getString(2);
+			System.out.println(sname);
+		}
+		
+		
+	}
+
+}
